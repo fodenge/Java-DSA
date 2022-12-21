dfe7a8fdca195c66772d60640677465e93fa5c9b
Shruti Pandey <120593611+fodenge@users.noreply.github.com>
2022-12-21 23:16:53 +0530

    Add files via upload

---
+++ Function/F_Marks.java
@@ -0,0 +1,42 @@
+package Function;
+
+import java.util.Scanner;
+
+import javax.lang.model.element.Element;
+
+public class F_Marks {
+    
+    // Write a program that will ask the user to enter his/her marks (out of 100). Define a method that will display grades according to the marks entered as below:
+    //                     Marks        Grade 
+    //                     91-100         AA 
+    //                     81-90          AB 
+    //                     71-80          BB 
+    //                     61-70          BC 
+    //                     51-60          CD 
+    //                     41-50          DD 
+    //                     <=40          Fail 
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter marks : ");
+        int marks = inp.nextInt();
+        String result = grade(marks);
+        System.out.println("Result = "+ result);
+    }
+    
+    static String grade(int marks){
+        if(marks >=91 && marks <= 100)
+           return "AA";
+        else if(marks >=81 && marks <= 90)
+           return "AB";
+        else if(marks >=71 && marks <= 80)
+           return "BB";
+        else if(marks >=61 && marks <= 70)
+           return "BC";
+        else if(marks >=51 && marks <= 60)
+           return "CD";
+        else if(marks >=41 && marks <= 50)
+           return "DD";
+        else return "Fail";
+    }
+
+}
